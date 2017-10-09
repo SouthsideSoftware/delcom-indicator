cfc189a2fb594adac85b5c057d674e642c81d961
Henry Golding <hegold@users.noreply.github.com>
2017-10-09 13:53:18 -0700

    Remove the need for sleep package by using mocha's done callback with setTimeout

--- tests/lightSpike.js
+++ tests/lightSpike.js
@@ -1,8 +1,7 @@
 var should = require('should');
-var sleep = require('sleep');
 var DelcomIndicator = require('../DelcomIndicator');
 
-const secondsToSleep = 4;
+const msToWait = 4000;
 
 describe('DelcomIndicator', function () {
 
@@ -14,7 +13,6 @@ describe('DelcomIndicator', function () {
   });
 
   afterEach(function () {
-    sleep.sleep(secondsToSleep);
     delcomIndicator.turnOff();
     delcomIndicator.close();
   });
@@ -23,33 +21,39 @@ describe('DelcomIndicator', function () {
     delcomIndicator.isConnected().should.equal(true, 'Delcom USB Indicator should be found')
   });
 
-  it('should set green solid led', function () {
+  it('should set green solid led', function (done) {
     delcomIndicator.solidGreen();
     console.log("Should be green");
+    setTimeout(done, msToWait);
   });
 
-  it('should set red solid led', function () {
+  it('should set red solid led', function (done) {
     delcomIndicator.solidRed();
     console.log("Should be red");
+    setTimeout(done, msToWait);
   });
 
-  it('should set blue or yellow solid led', function () {
+  it('should set blue or yellow solid led', function (done) {
     delcomIndicator.solidBlue();
     console.log("Should be blue or yellow");
+    setTimeout(done, msToWait);
   });
 
-  it('should set green flashing led', function () {
+  it('should set green flashing led', function (done) {
     delcomIndicator.flashGreen();
     console.log("Should be flashing green");
+    setTimeout(done, msToWait);
   });
 
-  it('should set red flashing led', function () {
+  it('should set red flashing led', function (done) {
     delcomIndicator.flashRed();
     console.log("Should be flashing red");
+    setTimeout(done, msToWait);
   });
 
-  it('should set blue or yellow flashing led', function () {
+  it('should set blue or yellow flashing led', function (done) {
     delcomIndicator.flashBlue();
     console.log("Should be flashing blue or yellow");
+    setTimeout(done, msToWait);
   });
 });
